ae8c28a2dd7b991d7b37e13aa63d69864da55d1c
ArjunKhatri5 <arjunkhatri21@maya.edu.in>
2024-09-15 16:24:19 +0530

    JS- seperate the output functionality into a function

--- script.js
+++ script.js
@@ -5,60 +5,27 @@ const reset = document.querySelector('.reset button');
 
 
 input.addEventListener('input', (e)=>{
-    let userInput = e.target.value;
+    let userInput = e.target.value.trim();
     wordCount.innerText = userInput.length;
-    if(Number(wordCount.innerText) < 100){
 
-        console.log("has attrib: ", input.hasAttribute('readOnly'));
-        console.log("remove attrib: ", input.removeAttribute('readOnly'));
-        
+    if(Number(wordCount.innerText) < 100){  
         input.hasAttribute('readOnly') && input.removeAttribute('readOnly');
-
-        output[0].innerText = userInput.toLowerCase();
-        output[1].innerText = userInput.toUpperCase();
-    
-        // camelCase
-        let camel = userInput.split(" ");   
-        if(camel[1]){   
-            for(let i = 1; i < camel.length; i++){
-                let str = camel[i];
-                camel[i] = str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
-            }
-        } else if(camel[1] === ''){
-            return;
-        }
-        output[2].innerText = camel.join(''); 
-    
-    
-        // PascalCase
-        let pascal = userInput.split(' ');
-        for(let i = 0; i < pascal.length; i++){
-            let str = pascal[i];
-            pascal[i] = str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
-        }
-        output[3].innerText = pascal.join('');
-    
-        
-        // snake_case
-        let snake = userInput.split(" ").join("_");
-        output[4].innerText = snake
-    
-    
-        // kebab-case
-        let kebab = userInput.split(" ").join("-");
-        output[5].innerText = kebab;
-    
-    
-        // trim
-        let trim = userInput.split(" ").join("");
-        output[6].innerText = trim;
+        showOutput(userInput)
     } else {
+        wordCount.innerText = 100;
         input.value = userInput.slice(0, 99);
+        showOutput(input.value);
+        input.hasAttribute('readOnly') || input.setAttribute('readOnly', true);
         reset.parentElement.classList.contains('hide') && reset.parentElement.classList.remove('hide');
     }
 });
 
 reset.addEventListener('click', ()=>{
+    clearInput();
+});
+
+
+function clearInput(){
     reset.parentElement.classList.contains('hide') || reset.parentElement.classList.add('hide');
     input.hasAttribute('readOnly') && input.removeAttribute('readOnly');
     input.value = '';
@@ -67,4 +34,46 @@ reset.addEventListener('click', ()=>{
     output.forEach((p) => {
         p.innerText = ''
     })
-});
+}
+
+
+function showOutput(userInput){
+    output[0].innerText = userInput.toLowerCase();
+    output[1].innerText = userInput.toUpperCase();
+
+    // camelCase
+    let camel = userInput.split(" ");   
+    if(camel[1]){   
+        for(let i = 1; i < camel.length; i++){
+            let str = camel[i];
+            camel[i] = str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+        }
+    } else if(camel[1] === ''){
+        return;
+    }
+    output[2].innerText = camel.join(''); 
+
+
+    // PascalCase
+    let pascal = userInput.split(' ');
+    for(let i = 0; i < pascal.length; i++){
+        let str = pascal[i];
+        pascal[i] = str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+    }
+    output[3].innerText = pascal.join('');
+
+    
+    // snake_case
+    let snake = userInput.split(" ").join("_");
+    output[4].innerText = snake
+
+
+    // kebab-case
+    let kebab = userInput.split(" ").join("-");
+    output[5].innerText = kebab;
+
+
+    // trim
+    let trim = userInput.split(" ").join("");
+    output[6].innerText = trim;
+}

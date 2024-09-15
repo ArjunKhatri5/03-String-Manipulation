97dc09c8334dfad69c8a1b3cb8257943f87e9c74
ArjunKhatri5 <arjunkhatri21@maya.edu.in>
2024-09-15 12:24:21 +0530

    limit input to 100 words and add word count functionality

--- script.js
+++ script.js
@@ -1,46 +1,60 @@
 const input = document.querySelector('.input input');
 const output = document.querySelectorAll('.output p');
 
+const wordCount = document.querySelector('.wordCount span');
+
 
 input.addEventListener('input', (e)=>{
     let userInput = e.target.value;
-    
-    output[0].innerText = userInput.toLowerCase();
-    output[1].innerText = userInput.toUpperCase();
+    wordCount.innerText = userInput.length;
+    if(Number(wordCount.innerText) < 100){
 
-    // camelCase
-    let camel = userInput.split(" ");   
-    if(camel[1]){   
-        for(let i = 1; i < camel.length; i++){
-            let str = camel[i];
-            camel[i] = str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+        console.log("has attrib: ", input.hasAttribute('readOnly'));
+        console.log("remove attrib: ", input.removeAttribute('readOnly'));
+        
+        input.hasAttribute('readOnly') && input.removeAttribute('readOnly');
+
+        output[0].innerText = userInput.toLowerCase();
+        output[1].innerText = userInput.toUpperCase();
+    
+        // camelCase
+        let camel = userInput.split(" ");   
+        if(camel[1]){   
+            for(let i = 1; i < camel.length; i++){
+                let str = camel[i];
+                camel[i] = str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+            }
+        } else if(camel[1] === ''){
+            return;
         }
-    } else if(camel[1] === ''){
-        return;
-    }
-    output[2].innerText = camel.join(''); 
-
-
-    // PascalCase
-    let pascal = userInput.split(' ');
-    for(let i = 0; i < pascal.length; i++){
-        let str = pascal[i];
-        pascal[i] = str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
-    }
-    output[3].innerText = pascal.join('');
-
+        output[2].innerText = camel.join(''); 
     
-    // snake_case
-    let snake = userInput.split(" ").join("_");
-    output[4].innerText = snake
+    
+        // PascalCase
+        let pascal = userInput.split(' ');
+        for(let i = 0; i < pascal.length; i++){
+            let str = pascal[i];
+            pascal[i] = str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+        }
+        output[3].innerText = pascal.join('');
+    
+        
+        // snake_case
+        let snake = userInput.split(" ").join("_");
+        output[4].innerText = snake
+    
+    
+        // kebab-case
+        let kebab = userInput.split(" ").join("-");
+        output[5].innerText = kebab;
+    
+    
+        // trim
+        let trim = userInput.split(" ").join("");
+        output[6].innerText = trim;
+    } else {
+        input.setAttribute('readOnly', true);
+    }
+    
 
-
-    // kebab-case
-    let kebab = userInput.split(" ").join("-");
-    output[5].innerText = kebab;
-
-
-    // trim
-    let trim = userInput.split(" ").join("");
-    output[6].innerText = trim;
 });
